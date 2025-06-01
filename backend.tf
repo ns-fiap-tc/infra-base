07c40d69b2ae3d4986c276af6f3db9fb5d27a9af
Nicolas Silva <nicolasoliveirasilva98@gmail.com>
2025-05-31 22:53:17 -0300

    fix: region on backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "lanchonete-tfstate-bucket"
     key            = "infra-base/terraform.tfstate"
-    region         = var.aws_region
+    region         = "us-east-1"
     dynamodb_table = "terraform-lock"
     encrypt        = true
   }
